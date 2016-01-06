06bbb021c09496eaf418e312a02d38766c2221d6
UKHYUN <UKHYUN@192.168.0.38>
2016-01-06 21:14:57 +0900

    MapCtrl.java 파일 추

--- WebContent/ROOT/googlemap.jsp
+++ WebContent/ROOT/googlemap.jsp
@@ -1,3 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"  import="sclab.db.*, java.util.*"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<% request.setCharacterEncoding("utf-8"); %>
+
+<jsp:useBean id="User" class="sclab.db.User"/>
+<jsp:useBean id="MapCtrl" class="sclab.db.MapCtrl"/> 
+
 <div id="map_canvas" style="width: 100%; height: 100%;" >
 	<script type="text/javascript"> /*initialize(0, 0); */ </script>
 	<script type="text/javascript" src='https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","controls","table"]}]}'></script>
---
+++ src/sclab/db/MapCtrl.java
@@ -0,0 +1,62 @@
+package sclab.db;
+
+import java.sql.*;
+import java.util.*;
+
+import sclab.db.DbConnector;
+
+public class MapCtrl {
+
+	DbConnector dbconnector = new DbConnector();
+	Connection conn = dbconnector.getConn();
+	PreparedStatement pstmt = dbconnector.getPstmt();
+
+	// OverUse flag가 1인 동을 받아옴 
+	public ArrayList<User> getOverUsedDongList() {
+
+		ArrayList<User> overUsedDongList = new ArrayList<User>();
+
+		String sql = "SELECT DISTINCT UMDONG FROM USER where code in (SELECT DISTINCT code FROM CONSUMPTION WHERE overused=1 and DATE='2015-03-01')";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				User user = new User();
+				user.setUmdong(rs.getString("umdong"));
+				overUsedDongList.add(user);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbconnector.disconnect();
+		}
+		return overUsedDongList;
+	}
+
+	//OverUse flag가 1인 동을 받아옴.
+	public ArrayList<User> getNormalDongList() {
+
+		ArrayList<User> normalDongList = new ArrayList<User>();
+
+		String sql = "SELECT DISTINCT UMDONG FROM USER where code in (SELECT DISTINCT code FROM CONSUMPTION WHERE overused=0 and DATE='2015-03-01')";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				User user = new User();
+				user.setUmdong(rs.getString("umdong"));
+				normalDongList.add(user);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbconnector.disconnect();
+		}
+		return normalDongList;
+	}
+
+}
