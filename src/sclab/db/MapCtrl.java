0eac447ca8611e0eb0b74805050486f611e356bd
UKHYUN <UKHYUN@192.168.0.38>
2016-01-07 16:08:56 +0900

    DB ing

--- src/sclab/db/MapCtrl.java
+++ src/sclab/db/MapCtrl.java
@@ -12,9 +12,9 @@ public class MapCtrl {
 	PreparedStatement pstmt = dbconnector.getPstmt();
 
 	// OverUse flag가 1인 동을 받아옴 
-	public ArrayList<User> getOverUsedDongList() {
+	public ArrayList<String> getOverUsedDongList() {
 
-		ArrayList<User> overUsedDongList = new ArrayList<User>();
+		ArrayList<String> overUsedDongList = new ArrayList<String>();
 
 		String sql = "SELECT DISTINCT UMDONG FROM USER where code in (SELECT DISTINCT code FROM CONSUMPTION WHERE overused=1 and DATE='2015-02-28')";
 
@@ -22,23 +22,22 @@ public class MapCtrl {
 			pstmt = conn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				User user = new User();
-				user.setUmdong(rs.getString("umdong"));
-				overUsedDongList.add(user);
+				String Umdong = new String();
+				Umdong = rs.getString("umdong");
+				overUsedDongList.add(Umdong);
 			}
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			dbconnector.disconnect();
 		}
+		
 		return overUsedDongList;
 	}
 
-	//OverUse flag가 1인 동을 받아옴.
-	public ArrayList<User> getNormalDongList() {
+	//OverUse flag가 0인 동을 받아옴.
+	public ArrayList<String> getNormalDongList() {
 
-		ArrayList<User> normalDongList = new ArrayList<User>();
+		ArrayList<String> normalDongList = new ArrayList<String>();
 
 		String sql = "SELECT DISTINCT UMDONG FROM USER where code in (SELECT DISTINCT code FROM CONSUMPTION WHERE overused=0 and DATE='2015-02-28')";
 
@@ -46,16 +45,16 @@ public class MapCtrl {
 			pstmt = conn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				User user = new User();
-				user.setUmdong(rs.getString("umdong"));
-				normalDongList.add(user);
+				String umDong = new String();
+				umDong = rs.getString("umdong");
+				normalDongList.add(umDong);
+				System.out.println(umDong);
 			}
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			dbconnector.disconnect();
 		}
+		
 		return normalDongList;
 	}
 
